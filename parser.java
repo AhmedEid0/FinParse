682de07f134d3270ece67571c3ec5edf1b67ef5b
AhmedEid0 <58207608+AhmedEid0@users.noreply.github.com>
2023-09-02 21:13:28 +0300

    Update parser.java

--- parser.java
+++ parser.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Main {
+public class parser {
  public static int sizefile(String fileName) throws FileNotFoundException {
   File file = new File(fileName);
   Scanner sc = new Scanner(file);
